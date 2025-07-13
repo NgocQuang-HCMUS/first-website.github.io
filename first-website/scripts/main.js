a564bf3c5cdbb6a87d2f76b0f7353c6db79ab681
Nguyễn Ngọc Quang <24120127@student.hcmus.edu.vn>
2025-07-13 18:43:34 +0700

    Add files via upload

---
+++ first-website/scripts/main.js
@@ -0,0 +1,29 @@
+const myImage = document.querySelector("img");
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Anime-girl-1.jpg") {
+    myImage.setAttribute("src", "images/Anime-girl-2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/Anime-girl-1.jpg");
+  }
+});
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `How are you today my bro, ${myName}`;
+  }
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.addEventListener("click", () => {
+  setUserName();
+});
